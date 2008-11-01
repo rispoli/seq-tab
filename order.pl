2bc1fbee84ec3aef11b03b0f68b1a51f6f0506d9
Daniele Rispoli <daniele.rispoli@gmail.com>
2008-11-01 11:28:41 +0100

    new file:   order.pl 		predicate to sort the propositions 	new file:   qsort.pl 		quicksort with custom predicate

---
+++ order.pl
@@ -0,0 +1,19 @@
+:- [qsort].
+
+order(l, L, L_o) :-
+	qsort(order_l, L, L_o).
+
+order(r, L, L_o) :-
+	qsort(order_r, L, L_o).
+
+%left
+order_l(_, _ # _).
+order_l(_, _ -> _).
+order_l(_ # _, _) :- !, fail.
+order_l(_ -> _, _) :- !, fail.
+order_l(A, B) :- A @=< B.
+
+%right
+order_r(_, _ & _).
+order_r(_ & _, _) :- !, fail.
+order_r(A, B) :- A @=< B.
